04349d765c5ff44b6fbf0f85003d38d3a20619ec
David Schwartz <davidschwartz23@gmail.com>
2019-06-28 22:38:46 -0700

    Added a few lines of code to the connection.js file

--- config/connection.js
+++ config/connection.js
@@ -18,20 +18,20 @@ if (process.env.q3vtafztappqbpzn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com) {
     });
 };
 
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+  
+  
 connection.connect();
 module.exports = connection;
 
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("burger_db", "root", "Chicago13", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+
 
 // Exports the connection for other files to use
 module.exports = sequelize;
