9ee5c71f5af91581f5d1959cee4354ef3890c857
David Schwartz <davidschwartz23@gmail.com>
2019-06-28 22:33:07 -0700

    Added require for handlebars along with app.engine and app.set

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Requiring our models for syncing
 var db = require("./models");
 
